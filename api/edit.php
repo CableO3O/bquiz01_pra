91a0044d44d9f1c7d59cf6da4efcfcaa5a162e42
cable <cable870526@gmail.com>
2024-02-02 09:34:37 +0800

    網站標題顯示、刪除功能

---
+++ api/edit.php
@@ -0,0 +1,22 @@
+<?php include_once "db.php";
+$table = $_POST['table'];
+$DB = ${ucfirst($table)};
+unset($_POST['table']);
+foreach ($_POST['id'] as $key => $id) {
+    dd($id);
+    if (isset($_POST['del']) && in_array($id, $_POST['del'])) {
+        $DB->del($id);
+    } else {
+        $row = $DB->find($id);
+        if (isset($row['text'])) {
+            $row['text'] = $_POST['text'][$key];
+        }
+        switch ($$table) {
+            case 'title':
+                $row['sh']=(isset($_POST['sh'])&&$_POST['sh']==$id)?1:0;
+                break;
+        }
+        $DB->save($row);
+    }
+}
+to("../back.php?do=$table");
--- back/title.php
+++ back/title.php
@@ -1,6 +1,6 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
     <p class="t cent botli">網站標題管理</p>
-    <form method="post" action="./api/main.php">
+    <form method="post" action="./api/edit.php">
         <table width="100%">
             <tbody>
                 <tr class="yel">
@@ -10,26 +10,36 @@
                     <td width="7%">刪除</td>
                     <td></td>
                 </tr>
-                <?php 
-$titles=$Title->all();
-foreach ($titles as $title) {
-    ?>
-                <tr>
-                    <td width="45%"><img src="./img/<?=$title['img'];?>" alt="" style="width: 300px;height:30px"></td>
-                    <td width="23%"><input type="text" name="" value="<?=$title['text'];?>"></td>
-                    <td width="7%"><input type="radio" name="sh[]" value="<?=$title['id'];?>"></td>
-                    <td width="7%"><input type="checkbox" name="del[]" value="<?=$title['id'];?>"></td>
-                    <td><input type="button" onclick="op('#cover','#cvr','.modal/update_title.php?modal=<?=$title['id'];?>')" value="更新圖片"></td>
-                </tr>
                 <?php
-            }
+                $titles = $Title->all();
+                foreach ($titles as $title) {
+                ?>
+                    <tr>
+                        <td width="45%"><img src="./img/<?= $title['img']; ?>" alt="" style="width: 300px;height:30px"></td>
+                        <td width="23%">
+                            <input type="text" name="text[]" value="<?= $title['text']; ?>">
+                            <input type="hidden" name="id[]" value="<?= $title['id']; ?>">
+                        </td>
+                        <td width="7%">
+                            <input type="radio" name="sh" value="<?= $title['id']; ?>" <?= ($title['sh'] == 1) ? 'checked' : ''; ?>>
+                        </td>
+                        <td width="7%">
+                            <input type="checkbox" name="del[]" value="<?= $title['id']; ?>">
+                        </td>
+                        <td>
+                            <input type="button" onclick="op('#cover','#cvr','.modal/update_title.php?modal=<?= $title['id']; ?>&&table=<?=$do;?>')" value="更新圖片">
+                        </td>
+                    </tr>
+                <?php
+                }
                 ?>
             </tbody>
         </table>
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>
-                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','.modal/<?=$do;?>.php?modal=<?=$do;?>')" value="新增網站標題圖片"></td>
+                    <input type="hidden" name="table" value="<?=$do;?>">
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','.modal/<?= $do; ?>.php?modal=<?= $do; ?>')" value="新增網站標題圖片"></td>
                     <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
                 </tr>
             </tbody>
